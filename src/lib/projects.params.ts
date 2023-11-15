2e1ecd4e5f3e8f141334fc9b432941983127329e
Mandy Mozart <mandymozart@viennastruggle.com>
2023-11-15 12:22:53 +0100

    example update

--- src/lib/projects.params.ts
+++ src/lib/projects.params.ts
@@ -4,36 +4,339 @@ import type { Project } from './types';
 
 const MY_PROJECTS: Array<Project> = [
 	{
-		slug: 'slick-portfolio-angular',
+		slug: 'swma',
 		color: '#5e95e3',
 		description:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore non dolores voluptatibus vitae praesentium aperiam, iure laboriosam repellendus sunt explicabo pariatur totam enim, nihil animi quisquam. Sit vero quod laborum!',
+			'Beautique shopify webshop for Stuttgart based product design company.',
 		shortDescription:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore non dolores voluptatibus vitae praesentium aperiam, iure laboriosam repellendus sunt explicabo pariatur totam enim, nihil animi quisquam. Sit vero quod laborum!',
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
+			'Beautique shopify webshop, marketing page and blog for Stuttgart based product design company.',
+		links: [{ to: 'https://www.swma.design', label: 'Website' }],
 		logo: Assets.Unknown,
-		name: 'Slick Portfolio',
+		name: 'Siegelwerk Manufactur',
 		period: {
-			from: new Date()
+			from: new Date(2018,2),
+			to: new Date(2019,9)
 		},
-		skills: getSkills('angular', 'ts', 'tailwind'),
-		type: 'Website Template'
+		skills: getSkills('shopify', 'js', 'sass'),
+		type: 'eCommerce'
 	},
 	{
-		slug: 'slick-portfolio-svelte',
+		slug: 'liah',
 		color: '#ff3e00',
 		description:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
+			'Complex customizer, marketing page and beautique WooCommerce shop for Zürich based furniture business.',
 		shortDescription:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
-		logo: Assets.Svelte,
-		name: 'Slick Portfolio',
+			'Complex customizer, marketing page and beautique WooCommerce shop for Zürich based furniture business.',
+		links: [{ to: 'https://www.liah.design', label: 'Website' }],
+		logo: Assets.Unknown,
+		name: 'LiAH',
 		period: {
-			from: new Date()
+			from: new Date(2020,1),
+			to: new Date(2020,7)
 		},
-		skills: getSkills('svelte', 'ts', 'tailwind', 'sass'),
-		type: 'Website Template',
+		skills: getSkills('woocommerce', 'react', 'sass'),
+		type: 'eCommerce',
+		screenshots: [
+			{
+				label: 'screen 1',
+				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '2',
+				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '3',
+				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '4',
+				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '5',
+				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '6',
+				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			}
+		]
+	},
+	{
+		slug: 'resradio-2.0',
+		color: '#ff3e00',
+		description:
+			'Complete radio station WebApp for Vienna based community radio.',
+		shortDescription:
+			'Complete radio station WebApp for Vienna based community radio.',
+		links: [{ to: 'https://www.res.radio', label: 'Website' }],
+		logo: Assets.Unknown,
+		name: 'Res Radio 2.0',
+		period: {
+			from: new Date(2022,3),
+			to: new Date(2022,4)
+		},
+		skills: getSkills('react', 'prismic', 'sanity','ably','ts','js'),
+		type: 'WebApp',
+		screenshots: [
+			{
+				label: 'screen 1',
+				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '2',
+				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '3',
+				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '4',
+				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '5',
+				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '6',
+				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			}
+		]
+	},
+	{
+		slug: 'resradio-1.0',
+		color: '#ff3e00',
+		description:
+			'Complete radio station WebApp for Vienna based community radio.',
+		shortDescription:
+			'Complete radio station WebApp for Vienna based community radio.',
+		links: [{ to: 'https://www.res.radio', label: 'Website' }],
+		logo: Assets.Unknown,
+		name: 'Res Radio 1.0',
+		period: {
+			from: new Date(2022,0),
+			to: new Date(2023,6)
+		},
+		skills: getSkills('react', 'prismic', 'sanity','ably','ts','js'),
+		type: 'WebApp',
+		screenshots: [
+			{
+				label: 'screen 1',
+				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '2',
+				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '3',
+				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '4',
+				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '5',
+				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '6',
+				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			}
+		]
+	},
+	{
+		slug: 'collisions-munich',
+		color: '#ff3e00',
+		description:
+			'Animated music festival & residency webapp.',
+		shortDescription:
+			'Animated avant-garde arts festival & residency about dark matter research in Munich (Germany) and Canfranc (Spain).',
+		links: [{ to: 'https://www.collisions.cc', label: 'Website' }],
+		logo: Assets.Unknown,
+		name: 'Collisions Munich',
+		period: {
+			from: new Date(2022,4),
+			to: new Date(2022,5)
+		},
+		skills: getSkills('react', 'pixiejs', 'ts','js'),
+		type: 'WebApp',
+		screenshots: [
+			{
+				label: 'screen 1',
+				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '2',
+				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '3',
+				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '4',
+				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '5',
+				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '6',
+				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			}
+		]
+	},
+	{
+		slug: 'collisions-canfranc',
+		color: '#ff3e00',
+		description:
+			'Animated music festival & residency webapp.',
+		shortDescription:
+			'Animated avant-garde arts festival & residency about dark matter research laboratorium Canfranc (Spain).',
+		links: [{ to: 'https://www.collisions.cc', label: 'Website' }],
+		logo: Assets.Unknown,
+		name: 'Collisions Canfranc',
+		period: {
+			from: new Date(2021,8),
+			to: new Date(2021,8)
+		},
+		skills: getSkills('react', 'pixiejs', 'ts','js'),
+		type: 'WebApp',
+		screenshots: [
+			{
+				label: 'screen 1',
+				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '2',
+				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '3',
+				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '4',
+				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '5',
+				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '6',
+				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			}
+		]
+	},
+	{
+		slug: 'rwa-email-editor',
+		color: '#ff3e00',
+		description:
+			'Interactive email template designer and versioning system for RWA and Lagerhaus Austria.',
+		shortDescription:
+			'In house Interactive email template designer and versioning system for RWA and Lagerhaus Austria.',
+		links: [{ to: 'https://rwa-emails.netlify.app/', label: 'WebApp' }],
+		logo: Assets.Unknown,
+		name: 'RWA Email Editor',
+		period: {
+			from: new Date(2022,0),
+			to: new Date(2023,0)
+		},
+		skills: getSkills('react', 'pixiejs', 'ts','js'),
+		type: 'WebApp',
+		screenshots: [
+			{
+				label: 'screen 1',
+				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '2',
+				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '3',
+				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '4',
+				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '5',
+				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '6',
+				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			}
+		]
+	},
+	{
+		slug: 'rwa-shop',
+		color: '#ff3e00',
+		description:
+			'B2B shop for largest agricultural retailer and cooperative in Austria.',
+		shortDescription:
+			'B2B shop for largest agricultural retailer and cooperative in Austria.',
+		links: [{ to: 'https://shop.rwa.at/', label: 'WebApp' }],
+		logo: Assets.Unknown,
+		name: 'RWA Shop',
+		period: {
+			from: new Date(2022,0),
+			to: new Date(2023,0)
+		},
+		skills: getSkills('react', 'hybris', 'ts','js', 'aws','netlify'),
+		type: 'eCommerce',
+		screenshots: [
+			{
+				label: 'screen 1',
+				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '2',
+				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '3',
+				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '4',
+				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '5',
+				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '6',
+				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			}
+		]
+	},
+	{
+		slug: 'strudle',
+		color: '#ff3e00',
+		description:
+			'Streaming platform.',
+		shortDescription:
+			'Streaming platform.',
+		links: [],
+		logo: Assets.Unknown,
+		name: 'Strudle',
+		period: {
+			from: new Date(2022,0),
+			to: new Date(2023,0)
+		},
+		skills: getSkills('react', 'python', 'django', 'ts','js'),
+		type: 'eCommerce',
 		screenshots: [
 			{
 				label: 'screen 1',
@@ -61,6 +364,7 @@ const MY_PROJECTS: Array<Project> = [
 			}
 		]
 	}
+	
 ];
 
 export default MY_PROJECTS;
